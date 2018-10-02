5224e36a0b7c0bcfc6896258deda8aa51b45d38a
AlecVercruysse <vercruysse.alec@gmail.com>
2018-10-02 13:45:00 -0700

    start sending/receiving data

--- radio_coms/radio_down.ino
+++ radio_coms/radio_down.ino
@@ -9,19 +9,37 @@
  *
  * Written by Alec Vercruysse
  *
- */
+ */ 
 
-final int setupmode = false //normal operation requires setupmode to be false
-
+int setupmode = false ; //normal operation requires setupmode to be false
+int shutdownpin = 22;
+int configpin = 24;
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);// for blinking internal LED
+    pinMode(shutdownpin, OUTPUT);
+    digitalWrite(shutdownpin, HIGH);
+
     Serial.begin(9600); //9600 Baud communication with computer
     Serial1.begin(9600);//9600 Baud communication with RF module
-
+    
     if (setupmode) {
-        Serial.println("Entering command mode with string: \"+++\"")
-        Serial1.write("+++");// "+++" enters command mode
-        Serial.println("System response: " + Serial1.readString())
+        Serial.println("Entering command mode by asserting config pin low and pulsing power (1s):");
+        digitalWrite(configpin, LOW);
+        digitalWrite(shutdownpin, LOW);
+        delay(2000);
+        digitalWrite(shutdownpin, HIGH);
+        Serial.println("Ok we should be in");
+        Serial.println("Getting current Destination Address [ATDT <Enter>]. Response:");
+        delay(100);
+        char command[] = {'A','T','D','T','\r'};
+        Serial1.write(command);
+        char response[10];
+        while (Serial1.available() > 0) {
+          Serial1.readBytes(response, 10);
+          Serial.println(response);
+        }
+        Serial.println("Done");
+        /**/
 
         /**
         Serial.println("Entering command mode with string: \"+++\"")
@@ -41,5 +59,10 @@ void loop() {
 
         digitalWrite(LED_BUILTIN, LOW);
         delay(1000);
+        Serial.println("Starting communication");
+        for (int i = 0; i < 1000; i++){
+          Serial1.write(i);
+          delay(200);
+        }
     }
 }
