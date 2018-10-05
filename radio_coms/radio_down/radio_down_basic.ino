285caefb289cf440756c9d4ff827bf77a84ecffd
Alec Vercruysse <vercruysse.alec@gmail.com>
2018-10-05 01:30:11 -0700

    got basic data to send

--- radio_coms/radio_down/radio_down_basic.ino
+++ radio_coms/radio_down/radio_down_basic.ino
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <math.h>       /* log10 */
+#include <stdlib.h>     /* atoi */
 
 int shutdownpin = 22;
 
@@ -11,11 +13,46 @@ void setup() {
 }
 
 void loop() {
+    /** This code works, sending byte values
     while (true) {
-        for (int i = 0; i < 255; i++) {
+        for (byte i = 0; i < 255; i++) {
             Serial.write(i);
             Serial1.write(i);
             delay(200);
         }
+    }
+     /**/
+    sendDataAsBytes();
+}
+
+void sendDataAsBytes(/*byte *dataArr, int dASize*/) {
+    byte start_sequence[] = {0, 0, 0, 0, 0, 0};
+    byte next_data_sequence[] = {6, 6, 6, 6, 6, 6};
+
+    //example data to send down:
+    String gps_lat = "23425634N";
+    String gps_long = "6586755E";
+    byte lat_arr[gps_lat.length()];
+    byte long_arr[gps_long.length()];
+    tBA(gps_lat, lat_arr);
+    tBA(gps_long, long_arr);
+
+    //send sequence:
+    txArr(start_sequence, 6);
+    txArr(lat_arr, gps_lat.length());
+    txArr(next_data_sequence, 6);
+    txArr(long_arr, gps_long.length());
+}
+
+void tBA(String n, byte *arr) {
+    for (int i = 0; i < n.length(); i++) {
+        byte b = int(n.charAt(i));
+        arr[i] = b;
+    }
+}
+
+void txArr(byte *buffer, int size) {
+    for (int i = 0; i < size; i++) {
+        Serial.write(buffer[i]);
     }
 }
--- radio_coms/radio_up/radio_up_basic.ino
+++ radio_coms/radio_up/radio_up_basic.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "../../../../../../usr/share/arduino-1.8.7/hardware/arduino/avr/cores/arduino/HardwareSerial.h"
+#include <QueueArray.h>
 
 int shutdownpin = 22;
 
@@ -9,11 +10,21 @@ void setup() {
 
     Serial.begin(9600); //9600 Baud communication with computer
     Serial1.begin(9600);//9600 Baud communication with RF module
+    Serial.write('\aa');
 }
 
+int last6[6] = {1, 2, 3, 4, 5, 6};
+int idx = 0;
+
+byte[] current
 void loop() {
     int incoming_data = Serial1.read();
-    if (incoming_data != -1) {
+    last6[idx] = incoming_data;
+    i = (i + 1) % 6;
+
+    /**if (incoming_data != -1) {
         Serial.println(incoming_data);
     }
+    /**/
+
 }
