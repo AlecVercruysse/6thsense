e5e1495afff43c6b6971d7f863466bd76b304c01
Luke Bowsher <luke.bowsher@menloschool.org>
2018-10-31 19:46:29 -0700

    took out simons geiger code, should be final update to code

--- secondary_arduino/secondary_arduino.ino
+++ secondary_arduino/secondary_arduino.ino
@@ -15,32 +15,29 @@ MPU6050 mpu6050(Wire);
 void setup() {
     Serial.begin(9600);
     OpenLog.begin(9600);
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
     delay(500);
     count = 0;
     accelCount = 0;
     timeElapsed = 0;
     mpu6050.begin();
     mpu6050.calcGyroOffsets(true);
-    //setupAccel();
-    digitalWrite(13, HIGH);
 }
 
 
 void loop() {
-    if (Serial.available() > 0) {
-      if (Serial.read() > -1) {
-        count++;
-      }
-    }
+//    if (Serial.available() > 0) {
+//      if (Serial.read() > -1) {
+//        count++;
+//      }
+//    }
     if (timeElapsed > 10000) {
-        OpenLog.print(millis());
-        OpenLog.print("CPM: ");
-        OpenLog.println(count * 6);
+        OpenLog.print("Millis: ");
+        OpenLog.println(millis());
+//        OpenLog.print("CPM: ");
+//        OpenLog.println(count * 6);
         timeElapsed = 0;
-        count = 0;
-    }
+//        count = 0;
+    } 
     mpu6050.update();
     accel_arr[accelCount] = getTheAccel();
     accelCount ++;
